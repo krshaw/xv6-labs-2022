82bae2280d7e4f0a1661acabb866b5dafdd3c449
Kevin Shaw <kshaw2998@gmail.com>
2023-03-25 18:27:56 -0700

    mostly working, need to fix multiline

--- user/xargs.c
+++ user/xargs.c
@@ -3,50 +3,53 @@
 #include "user/user.h"
 
 void
-append(char *res[], char *A[], int a_len, char B[MAXARG][512], int b_len) {
+append(char *res[], char *A[], int a_len, char *B[], int b_len) {
     for (int i = 0; i < a_len; i++) res[i] = A[i];
     for (int i = 0; i < b_len; i++) res[i+a_len - 1] = B[i];
 }
 
 void
-split(char strs[MAXARG][512], char *str, char delim, int* c) {
+split(char *strs[], char *str, char delim, int* c) {
     int i = 0;
     int j = 0;
+    strs[i] = malloc(512 * sizeof(char));
     //char single_str[512];
     char *p = str;
     while (*p) {
         if (*p == delim) {
             strs[i][j++] = 0;
             j = 0;
-            i++;
+            strs[++i] = malloc(512 * sizeof(char));
             p++;
             continue;
         }
-        //single_str[j++] = *p++;
         strs[i][j++] = *p++;
     }
-    *c = i;
+    strs[i][j++] = 0;
+    *c = i + 1;
 }
 
-void
-apply(char *command[], char* additional_args, int argc) {
-
-    //printf("applying command with %s\n", additional_args);
-    // split additional_args by space
+void apply(char *command[], char* additional_args, int argc) {
     char *args[MAXARG];
     int more_argc;
-    char more_args[MAXARG][512];
+    char *more_args[MAXARG];
     split(more_args, additional_args, ' ', &more_argc);
-    for (int i = 0; i < more_argc; i++) printf("%s\n", more_args[i]);
-    //printf("MORE_ARGC: %d\n", more_argc);
+
+    //for (int i = 0; i < more_argc; i++) printf("%s\n", more_args[i]);
     if (argc - 1 + more_argc > MAXARG) {
         fprintf(2, "too many args!\n");
     }
-    append(args, command + 1, argc - 1, more_args, more_argc);
- 
+    //for (int i = 0; i < argc; i++) printf("%s ", command[i]);
+    //for (int i = 0; i < more_argc; i++) printf("%s ", more_args[i]);
+    //printf("\n");
+    append(args, command, argc, more_args, more_argc);
+    args[argc-1+more_argc] = 0;
+    //printf("running command:\n");
+    //for (int i = 0; i < argc + more_argc - 1; i++) printf("%s ", args[i]);
+    //printf("\n");
     if (fork() == 0) {
         // child
-        printf("%s\n", command[0]);
+        //for (int i = 0; i < argc - 1 + more_argc; i++) printf("%s\n", args[i]);
         exec(command[0], args);
         printf("exec error\n");
     } else {
@@ -54,6 +57,7 @@ apply(char *command[], char* additional_args, int argc) {
         exit(0);
     }
 }
+
 
 int
 main(int argc, char *argv[])
@@ -64,21 +68,22 @@ main(int argc, char *argv[])
   }
   // find all files in a directory with a given name
   char buf[512];
-  int n = read(0, buf, sizeof buf);
-  buf[n] = 0;
+  int i = 0;
+  while(read(0, &buf[i++], 1) > 0) {}
   char additional_args[512];
   int j = 0;
-  char **command = argv + 1;
+  char **command = &argv[1];
   char *p = buf;
+
   while (*p) {
     if (*p == '\n') {
         additional_args[j] = 0;
         j = 0;
-        //printf("HERE\n");
         apply(command, additional_args, argc);
         p++;
         continue;
     }
+    //printf("%s\n", additional_args);
     additional_args[j++] = *p++;
   }
   exit(0);
